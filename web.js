817aff11a8b9f85e88ab474516061eda0c178924
Tudor V <ubuntu@ip-172-31-15-12.eu-west-1.compute.internal>
2013-07-10 21:09:42 +0000

    modif & fix to read from index.html

--- web.js
+++ web.js
@@ -4,7 +4,13 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   /*response.send('Hello World 2!');*/
-    response.send(__dirname + '/index.html');
+  /*response.send(__dirname + '/index.html');*/
+    fs.readFile(__dirname + '/index.html', function (err, data) {
+	if (err) throw err;
+	//console.log(data);
+	response.send(data.toString());
+    });
+
 });
 
 var port = process.env.PORT || 5000;
